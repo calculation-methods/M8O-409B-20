a1d3df8db04ccc1edbaf3ad5eb87c7b4d888b868
MRChibis <86952249+MRChibis@users.noreply.github.com>
2023-12-18 22:13:04 +0300

    Add files via upload

---
+++ cursework/generation_matrix_nxn.py
@@ -0,0 +1,30 @@
+import argparse
+from random import randint
+
+import numpy as np
+from scipy.sparse import csc_matrix, rand
+
+
+parser = argparse.ArgumentParser()
+
+output = "matrix100.txt"
+shape = int(input())
+if shape < 3:
+    exit()
+
+matrix = rand(shape, shape, density=0.4, random_state=randint(112, 154))
+matrix = matrix.toarray()
+for i in range(shape):
+    for j in range(shape):
+        matrix[j][i] = matrix[i][j]
+matrix = csc_matrix(matrix)
+with open(output, "w") as f:
+    f.write(f"{shape}\n")
+    for i in matrix.toarray().round(3):
+        for j in i:
+            f.write(f"{j} ")
+        f.write("\n")
+    d = np.random.randint(5, 53, shape)
+    for i in d:
+        f.write(f"{i} ")
+    f.write("\n")
